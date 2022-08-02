3137f0148cb22912e9f9c19566e21ea851c3814e
unknown <obaicaleb@gmail.com>
2022-08-02 18:32:32 +0300

    fixed VM

---
+++ MyCoffee/MyCoffee/Models/Coffee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCoffee.Models
+{
+    internal class Coffee
+    {
+    }
+}
--- MyCoffee/MyCoffee/ViewModels/CoffeEquipmentViewModel.cs
+++ MyCoffee/MyCoffee/ViewModels/CoffeEquipmentViewModel.cs
@@ -25,10 +25,17 @@ namespace MyCoffee.ViewModels
 
             Title = "Coffee Equipment";
 
-            var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min";
+            var image = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia-cldnry.s-nbcnews.com%2Fimage%2Fupload%2Ft_nbcnews-fp-1200-630%2Cf_auto%2Cq_auto%3Abest%2Fnewscms%2F2019_33%2F2203981%2F171026-better-coffee-boost-se-329p.jpg&imgrefurl=https%3A%2F%2Fwww.nbcnews.com%2Fbetter%2Flifestyle%2Fhow-tap-health-benefits-coffee-ncna1096031&tbnid=Ca2oyrpKXFdGeM&vet=12ahUKEwjFoMHyuKj5AhVL9IUKHYkODSoQMygBegUIARDhAQ..i&docid=L4W4bO3khfL-_M&w=1200&h=630&q=coffee&ved=2ahUKEwjFoMHyuKj5AhVL9IUKHYkODSoQMygBegUIARDhAQ";
 
             Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
             Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
             Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "KKH", Image = image });
 
             CoffeeGroups.Add(new Grouping<string, Coffee>("Blue Bottle", Coffee.Where(c => c.Roaster == "Blue Bottle")));
